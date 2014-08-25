64035477e453efd79f7db83429a95313c0198510
hans.thunberg@callistaenterprise.se <hans.thunberg@callistaenterprise.se@95a8c5e1-5168-66ac-ef81-70fca3c1b79b>
2014-08-25 12:08:45 +0000

    [maven-scm] copy for tag GetAggregatedMaternityMedicalHistory-1.0.1-RC3

---
+++ trunk/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/RequestListFactoryImpl.java
@@ -0,0 +1,126 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistory;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.soitoolkit.commons.mule.util.ThreadSafeSimpleDateFormat;
+
+import se.riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistoryresponder.v2.GetMaternityMedicalHistoryType;
+import se.skltp.agp.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentResponseType;
+import se.skltp.agp.riv.itintegration.engagementindex.v1.EngagementType;
+import se.skltp.agp.service.api.QueryObject;
+import se.skltp.agp.service.api.RequestListFactory;
+
+public class RequestListFactoryImpl implements RequestListFactory {
+
+    private static final Logger log = LoggerFactory.getLogger(RequestListFactoryImpl.class);
+    private static final ThreadSafeSimpleDateFormat dtf = new ThreadSafeSimpleDateFormat("yyyyMMddHHmmss");
+    private static final ThreadSafeSimpleDateFormat df = new ThreadSafeSimpleDateFormat("yyyyMMdd");
+    
+    /**
+     * Filtrera svarsposter från i EI (ei-engagement) baserat parametrar i GetAggregatedMaternityMedicalHistory requestet (req).
+     * Följande villkor måste vara sanna för att en svarspost från EI skall tas med i svaret:
+     * 
+     * 1. req.timePeriod.start <= ei-engagement.mostRecentContent <= req.timePeriod.end
+     * 
+     * Ett anrop görs per funnet sourceSystem med följande värden i anropet:
+     * 
+     * 1. logicalAddress = logicalAddress (sourceSystem kan fungera vid systemadressering men bättre att alltid använda logiaclAddress)
+     * 2. subjectOfCareId = orginal-request.subjectOfCareId
+     * 3. careUnitId = orginal-request.careUnitId
+     * 4. fromDate = orginal-request.fromDate
+     * 5. toDate = orginal-request.toDate
+     */
+    @Override
+    public List<Object[]> createRequestList(QueryObject qo, FindContentResponseType src) {
+
+        GetMaternityMedicalHistoryType originalRequest = (GetMaternityMedicalHistoryType)qo.getExtraArg();
+        Date reqFrom = null;
+        Date reqTo = null;
+
+        if(originalRequest.getTimePeriod() != null){
+            reqFrom = parseTs(originalRequest.getTimePeriod().getStart());
+            reqTo   = parseTs(originalRequest.getTimePeriod().getEnd());	
+        }
+
+        FindContentResponseType eiResp = (FindContentResponseType)src;
+        List<EngagementType> inEngagements = eiResp.getEngagement();
+
+        log.info("Got {} hits in the engagement index", inEngagements.size());
+
+        Set<String> sourceSystems = new HashSet<String>();
+
+        for (EngagementType inEng : inEngagements) {
+
+            // Filter
+            if (isBetween(reqFrom, reqTo, inEng.getMostRecentContent())) {
+
+                log.info("Add SS: {}", inEng.getLogicalAddress());
+                sourceSystems.add(inEng.getLogicalAddress());
+            }
+        }
+
+        // Prepare the result of the transformation as a list of request-payloads, 
+        // one payload for each unique logical-address (e.g. source system since we are using systemaddressing),
+        // each payload built up as an object-array according to the JAX-WS signature for the method in the service interface
+        List<Object[]> reqList = new ArrayList<Object[]>();
+
+        for (String sourceSystem: sourceSystems) {
+
+            if (log.isInfoEnabled()) log.info("Calling source system using logical address {} for subject of care id {}", sourceSystem, originalRequest.getPatientId().getId());
+
+            GetMaternityMedicalHistoryType request = new GetMaternityMedicalHistoryType();
+            request.setPatientId(originalRequest.getPatientId());
+
+            // Don't set by PDL-enhet (logicalAddress)
+            if(originalRequest.getCareUnitHSAid() != null && originalRequest.getCareUnitHSAid().size() > 0){
+                request.getCareUnitHSAid().addAll(originalRequest.getCareUnitHSAid()); 
+            }
+
+            request.setTimePeriod(originalRequest.getTimePeriod());
+
+            Object[] reqArr = new Object[] {sourceSystem, request};
+
+            reqList.add(reqArr);
+        }
+
+        log.debug("Transformed payload: {}", reqList);
+
+        return reqList;
+    }
+
+    protected Date parseTs(String ts) {
+        try {
+            if (ts == null || ts.length() == 0) {
+                return null;
+            } else {
+                return df.parse(ts);
+            }
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Not in use in this service domain
+    protected boolean isBetween(Date from, Date to, String tsStr) {
+        return true;
+    }
+    
+/*    protected boolean isBetween(Date from, Date to, String tsStr) {
+        try {
+            log.debug("Is {} between {} and {}", new Object[]{tsStr, from, to});
+            Date ts = dtf.parse(tsStr);
+            if (from != null && from.after(ts)) return false;
+            if (to != null && to.before(ts)) return false;
+            return true;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }*/
+}
---
+++ trunk/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/integrationtest/MaternityMedicalHistoryTestProducerDb.java
@@ -0,0 +1,156 @@
+package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistory.integrationtest;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import se.riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistoryresponder.v2.GetMaternityMedicalHistoryResponseType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.CVType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.HealthcareProfessionalType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.LegalAuthenticatorType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.MaternityMedicalRecordBodyType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.MaternityMedicalRecordType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.OrgUnitType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.PQType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.PatientSummaryHeaderType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.PersonIdType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.PregnancyCheckupRecordType;
+import se.riv.clinicalprocess.healthcond.actoutcome.v2.RegistrationRecordType;
+import se.skltp.agp.test.producer.TestProducerDb;
+
+public class MaternityMedicalHistoryTestProducerDb extends TestProducerDb {
+
+    private static final Logger log = LoggerFactory.getLogger(MaternityMedicalHistoryTestProducerDb.class);
+
+    @Override
+    public Object createResponse(Object... responseItems) {
+        log.debug("Creates a response with {} items", responseItems);
+        GetMaternityMedicalHistoryResponseType response = new GetMaternityMedicalHistoryResponseType();
+        for (int i = 0; i < responseItems.length; i++) {
+            response.getMaternityMedicalRecord().add((MaternityMedicalRecordType)responseItems[i]);
+        }
+        return response;
+    }
+
+    public static final String TEST_REASON_DEFAULT = "default reason";
+    public static final String TEST_REASON_UPDATED = "updated reason";
+
+    @Override
+    public Object createResponseItem(String logicalAddress, String registeredResidentId, String businessObjectId, String time) {
+
+        log.debug("Created one response item for logical-address {}, registeredResidentId {} and businessObjectId {}",
+                new Object[] {logicalAddress, registeredResidentId, businessObjectId});
+
+        MaternityMedicalRecordType response = new MaternityMedicalRecordType();
+        response.setMaternityMedicalRecordHeader(generatePatientSummaryHeader(logicalAddress, businessObjectId, registeredResidentId, time));
+        response.setMaternityMedicalRecordBody(generateRecordBody());
+        return response;
+    }
+	
+	
+    protected PQType buildPQ(String unit, double value) {
+		PQType pq = new PQType();
+		pq.setUnit(unit);
+		pq.setValue(value);
+		return pq;
+    }
+    
+    protected MaternityMedicalRecordBodyType generateRecordBody() {
+    	MaternityMedicalRecordBodyType body = new MaternityMedicalRecordBodyType();
+    	PregnancyCheckupRecordType pregnancyCheckUpRecord = new PregnancyCheckupRecordType();
+    	{
+    		pregnancyCheckUpRecord.setProteinuria(buildPQ("mg/dL", 30.0));
+    		pregnancyCheckUpRecord.setBloodPressureDiastolic(buildPQ("mm Hg", 83.7));
+    		pregnancyCheckUpRecord.setBloodPressureSystolic(buildPQ("mm Hg", 135.2));
+    		pregnancyCheckUpRecord.setCompleteWeeksOfGestation(22);
+    		pregnancyCheckUpRecord.getFetalHeartRate().add(buildPQ("bpm", 82));
+    		pregnancyCheckUpRecord.getFetalPosition().add(new BigInteger("1"));
+    		pregnancyCheckUpRecord.getFetalPresentation().add(new BigInteger("1"));
+    		pregnancyCheckUpRecord.setGlycosuria(buildPQ("mg/dL", 250.0));
+    		pregnancyCheckUpRecord.setHaemoglobin(buildPQ("g/dL", 14.3));
+    		pregnancyCheckUpRecord.setSymphysisFundalHeight(buildPQ("cm", 20.0));
+    		pregnancyCheckUpRecord.getTypeOfLeave().add(new BigInteger("0"));
+    		pregnancyCheckUpRecord.setWeight(buildPQ("g", 301.0));
+    	}
+    	body.setPregnancyCheckupRecord(pregnancyCheckUpRecord);
+    	body.setPostDeliveryRecord(null);
+    	RegistrationRecordType registrationRecord = new RegistrationRecordType();
+    	{
+	    	registrationRecord.setAssessmentAtFirstContactStandardCare(true);
+	    	registrationRecord.setContraceptiveDiscontinued("20130401");
+	    	registrationRecord.setDiseasesDiabetesMellitus(true);
+	    	registrationRecord.setDiseasesEndocineDiseases(false);
+	    	registrationRecord.setDiseasesRecurrentUrinaryTractInfections(false);
+	    	registrationRecord.setDiseasesThrombosis(false);
+	    	registrationRecord.setExpectedDayOfDeliveryFromEmbryonicTransfer(null);
+	    	registrationRecord.setExpectedDayOfDeliveryFromLastMenstrualPeriod("20130208");
+	    	registrationRecord.setExpectedDayOfDeliveryFromUltrasoundScan(null);
+	    	registrationRecord.setIndicationPregnancy("20130610");
+	    	registrationRecord.setInfertility(new BigDecimal(0.0));
+	    	registrationRecord.setLastMenstrualPeriod("20130503");
+	    	registrationRecord.setWeight(buildPQ("kg", 69));
+    	}
+    	body.setRegistrationRecord(registrationRecord);
+    	return body;
+	}
+
+	protected PatientSummaryHeaderType generatePatientSummaryHeader(String logicalAddress, String businessObjectId, String registeredResidentId, String time) {
+    	PatientSummaryHeaderType header = new PatientSummaryHeaderType();
+    	{
+	    	header.setApprovedForPatient(true);
+	    	HealthcareProfessionalType author = new HealthcareProfessionalType();
+	    	{
+	    		author.setAuthorTime("20130707010203");
+	    		author.setHealthcareProfessionalCareGiverHSAId("HSA-SLL-01");
+	    		author.setHealthcareProfessionalCareUnitHSAId(logicalAddress + "-careUnit");
+	    		author.setHealthcareProfessionalHSAId("HSA-BMOR-1");
+	    		author.setHealthcareProfessionalName("Ulla Bandage");
+	    		OrgUnitType orgUnit = new OrgUnitType();
+	    		orgUnit.setOrgUnitAddress("Bättringsvägen 1");
+	    		orgUnit.setOrgUnitHSAId(logicalAddress + "-orgUnit");
+		        if(TestProducerDb.TEST_LOGICAL_ADDRESS_1.equals(logicalAddress)){
+		            orgUnit.setOrgUnitName("Vårdcentralen Kusten, Kärna");
+		        } else if(TestProducerDb.TEST_LOGICAL_ADDRESS_2.equals(logicalAddress)){
+		        	orgUnit.setOrgUnitName("Vårdcentralen Molnet");
+		        } else {
+		        	orgUnit.setOrgUnitName("Vårdcentralen Stacken");
+		        }	    		
+	    		author.setHealthcareProfessionalOrgUnit(orgUnit);
+	    		CVType roleCode = new CVType();
+	    		roleCode.setCode("205011");
+	    		roleCode.setCodeSystem("OID 1.2.752.129.2.2.1.4");
+	    		roleCode.setCodeSystemName("KV Befattning");
+	    		roleCode.setCodeSystemVersion("3.2");
+	    		roleCode.setDisplayName("Barnmorska, mottagning/rådgivning");
+	    		roleCode.setOriginalText("Barnmorska, mottagning/rådgivning");
+	    		author.setHealthcareProfessionalRoleCode(roleCode);
+	    	}
+	    	header.setAccountableHealthcareProfessional(author);	    	
+	    	header.setDocumentTime("20130707010203");
+			
+	        header.setCareContactId(businessObjectId);
+	    	header.setDocumentId("MATER-" + (int)(Math.random()*10000));
+	    	header.setDocumentTime("20130707010203");
+	    	LegalAuthenticatorType legalAuthenticator = new LegalAuthenticatorType();
+	    	{
+		    	legalAuthenticator.setLegalAuthenticatorHSAId("HSA-BMOR-1");
+		    	legalAuthenticator.setSignatureTime("20130707112233");
+	    	}
+	    	header.setLegalAuthenticator(legalAuthenticator);
+	    	PersonIdType patientId = new PersonIdType();
+	    	{
+		    	patientId.setId(registeredResidentId);
+				patientId.setType("1.2.752.129.2.1.3.1");
+	    	}
+			header.setPatientId(patientId);
+	    	
+	    	header.setSourceSystemHSAId(logicalAddress);
+			
+    	}
+    	return header;
+    }
+	
+	
+}
