c09a1b8a9bc813b0bf579bef9b8144ccd54e7f3e
Martin Flower <martin.flower@callistaenterprise.se>
2015-03-23 11:12:47 +0100

    SERVICE-155 - filter by SourceSystemHSAId

--- GetAggregatedMaternityMedicalHistory/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/RequestListFactoryImpl.java
+++ GetAggregatedMaternityMedicalHistory/src/main/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/RequestListFactoryImpl.java
@@ -3,13 +3,16 @@ package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.g
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.Map.Entry;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.soitoolkit.commons.mule.util.ThreadSafeSimpleDateFormat;
 
 import riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistoryresponder.v2.GetMaternityMedicalHistoryType;
 import se.skltp.agp.riv.itintegration.engagementindex.findcontentresponder.v1.FindContentResponseType;
@@ -20,49 +23,37 @@ import se.skltp.agp.service.api.RequestListFactory;
 public class RequestListFactoryImpl implements RequestListFactory {
 
     private static final Logger log = LoggerFactory.getLogger(RequestListFactoryImpl.class);
-    private static final ThreadSafeSimpleDateFormat dtf = new ThreadSafeSimpleDateFormat("yyyyMMddHHmmss");
-    private static final ThreadSafeSimpleDateFormat df = new ThreadSafeSimpleDateFormat("yyyyMMdd");
 
     /**
-     * Filtrera svarsposter från i EI (ei-engagement) baserat parametrar i GetAggregatedMaternityMedicalHistory requestet (req).
+     * Filtrera svarsposter från i EI (ei-engagement) baserat parametrar i GetReferralOutcome requestet (req).
      * Följande villkor måste vara sanna för att en svarspost från EI skall tas med i svaret:
      *
-     * 1. req.timePeriod.start <= ei-engagement.mostRecentContent <= req.timePeriod.end
+     * 1. req.fromDate <= ei-engagement.mostRecentContent <= req.toDate
+     * 2. req.careUnitId.size == 0 or req.careUnitId.contains(ei-engagement.logicalAddress)
+     *
+     * Svarsposter från EI som passerat filtreringen grupperas på fältet sourceSystem samt postens fält logicalAddress (= PDL-enhet) samlas i listan careUnitId per varje sourceSystem
      *
      * Ett anrop görs per funnet sourceSystem med följande värden i anropet:
      *
-     * 1. logicalAddress = logicalAddress (sourceSystem kan fungera vid systemadressering men bättre att alltid använda logiaclAddress)
+     * 1. logicalAddress = sourceSystem (systemadressering)
      * 2. subjectOfCareId = orginal-request.subjectOfCareId
-     * 3. careUnitId = orginal-request.careUnitId
-     * 4. fromDate = orginal-request.fromDate
-     * 5. toDate = orginal-request.toDate
+     * 3. careUnitId = listan av PDL-enheter som returnerats från EI för aktuellt source system)
      */
-    @Override
     public List<Object[]> createRequestList(QueryObject qo, FindContentResponseType src) {
-
-        GetMaternityMedicalHistoryType originalRequest = (GetMaternityMedicalHistoryType)qo.getExtraArg();
-        Date reqFrom = null;
-        Date reqTo = null;
-
-        if(originalRequest.getTimePeriod() != null){
-            reqFrom = parseTs(originalRequest.getTimePeriod().getStart());
-            reqTo   = parseTs(originalRequest.getTimePeriod().getEnd());
-        }
+        final GetMaternityMedicalHistoryType originalRequest = (GetMaternityMedicalHistoryType)qo.getExtraArg();
+        final String reqCareUnit = originalRequest.getSourceSystemHSAId();
 
         FindContentResponseType eiResp = (FindContentResponseType)src;
         List<EngagementType> inEngagements = eiResp.getEngagement();
 
         log.info("Got {} hits in the engagement index", inEngagements.size());
 
-        Set<String> sourceSystems = new HashSet<String>();
+        Map<String, List<String>> sourceSystem_pdlUnitList_map = new HashMap<String, List<String>>();
 
         for (EngagementType inEng : inEngagements) {
-
-            // Filter
-            if (isBetween(reqFrom, reqTo, inEng.getMostRecentContent())) {
-
-                log.info("Add SS: {}", inEng.getLogicalAddress());
-                sourceSystems.add(inEng.getLogicalAddress());
+            if (isPartOf(reqCareUnit, inEng.getLogicalAddress())) {
+                log.debug("Add SS: {} for PDL unit: {}", inEng.getSourceSystem(), inEng.getLogicalAddress());
+                addPdlUnitToSourceSystem(sourceSystem_pdlUnitList_map, inEng.getSourceSystem(), inEng.getLogicalAddress());
             }
         }
 
@@ -70,57 +61,34 @@ public class RequestListFactoryImpl implements RequestListFactory {
         // one payload for each unique logical-address (e.g. source system since we are using systemaddressing),
         // each payload built up as an object-array according to the JAX-WS signature for the method in the service interface
         List<Object[]> reqList = new ArrayList<Object[]>();
+        for (Entry<String, List<String>> entry : sourceSystem_pdlUnitList_map.entrySet()) {
+            final String sourceSystem = entry.getKey();
+            final GetMaternityMedicalHistoryType request = originalRequest;
 
-        for (String sourceSystem: sourceSystems) {
-
-            if (log.isInfoEnabled()) log.info("Calling source system using logical address {} for subject of care id {}", sourceSystem, originalRequest.getPatientId().getId());
-
-            GetMaternityMedicalHistoryType request = new GetMaternityMedicalHistoryType();
-            request.setPatientId(originalRequest.getPatientId());
-
-            // Don't set by PDL-enhet (logicalAddress)
-            if(originalRequest.getCareUnitHSAid() != null && originalRequest.getCareUnitHSAid().size() > 0){
-                request.getCareUnitHSAid().addAll(originalRequest.getCareUnitHSAid());
+            if(log.isInfoEnabled()) {
+                log.info("Calling source system using logical address {} for subject of care {}", sourceSystem, originalRequest.getPatientId().getId());
             }
-
-            request.setTimePeriod(originalRequest.getTimePeriod());
-
+            
             Object[] reqArr = new Object[] {sourceSystem, request};
-
             reqList.add(reqArr);
         }
-
         log.debug("Transformed payload: {}", reqList);
-
         return reqList;
     }
-
-    protected Date parseTs(String ts) {
-        try {
-            if (ts == null || ts.length() == 0) {
-                return null;
-            } else {
-                return df.parse(ts);
-            }
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
+    
+    boolean isPartOf(final String careUnitId, final String careUnit) {
+        log.debug("Check careunit {} equals expected {}", careUnitId, careUnit);
+        if(StringUtils.isBlank(careUnitId)) return true;
+        return careUnitId.equals(careUnit);
     }
 
-    // Not in use in this service domain
-    protected boolean isBetween(Date from, Date to, String tsStr) {
-        return true;
+    void addPdlUnitToSourceSystem(Map<String, List<String>> sourceSystem_pdlUnitList_map, String sourceSystem, String pdlUnitId) {
+        List<String> careUnitList = sourceSystem_pdlUnitList_map.get(sourceSystem);
+        if (careUnitList == null) {
+            careUnitList = new ArrayList<String>();
+            sourceSystem_pdlUnitList_map.put(sourceSystem, careUnitList);
+        }
+        careUnitList.add(pdlUnitId);
     }
 
-/*    protected boolean isBetween(Date from, Date to, String tsStr) {
-        try {
-            log.debug("Is {} between {} and {}", new Object[]{tsStr, from, to});
-            Date ts = dtf.parse(tsStr);
-            if (from != null && from.after(ts)) return false;
-            if (to != null && to.before(ts)) return false;
-            return true;
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        }
-    }*/
 }
