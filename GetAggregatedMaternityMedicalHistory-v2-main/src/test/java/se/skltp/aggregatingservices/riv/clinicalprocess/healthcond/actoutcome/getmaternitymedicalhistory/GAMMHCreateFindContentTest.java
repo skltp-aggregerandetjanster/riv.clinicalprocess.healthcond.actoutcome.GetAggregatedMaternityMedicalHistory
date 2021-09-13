884f24ac93c8e88f79cf1fffc037749c1b6a590d
martul <mariia.tuliakova@soprasteria.com>
2021-09-13 14:34:14 +0200

    Anpassning för camel 3

--- GetAggregatedMaternityMedicalHistory-v2-main/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/GAMMHCreateFindContentTest.java
+++ GetAggregatedMaternityMedicalHistory-v2-main/src/test/java/se/skltp/aggregatingservices/riv/clinicalprocess/healthcond/actoutcome/getmaternitymedicalhistory/GAMMHCreateFindContentTest.java
@@ -1,14 +1,12 @@
 package se.skltp.aggregatingservices.riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistory;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import riv.clinicalprocess.healthcond.actoutcome.getmaternitymedicalhistoryresponder.v2.GetMaternityMedicalHistoryResponseType;
 import se.skltp.aggregatingservices.api.AgpServiceFactory;
 import se.skltp.aggregatingservices.tests.CreateFindContentTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
+@ExtendWith(SpringExtension.class)
 public class GAMMHCreateFindContentTest extends CreateFindContentTest {
 
   private static GAMMHAgpServiceConfiguration configuration = new GAMMHAgpServiceConfiguration();
